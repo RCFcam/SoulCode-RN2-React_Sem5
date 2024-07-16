891e5a23129284ca158d0842f8d86da526296757
Bowie <marketing@letscode.com.br>
2024-07-16 08:41:38 -0300

    sem arquivo fire

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,6 @@ import { getFirestore } from "firebase/firestore";
 
 //
 const firebaseConfig = {
-  apiKey: "AIzaSyBaLugrW4T-_HH07dj_-15mCrtt2H6axGI",
-  authDomain: "my-taskro.firebaseapp.com",
-  projectId: "my-taskro",
-  storageBucket: "my-taskro.appspot.com",
-  messagingSenderId: "408771440842",
-  appId: "1:408771440842:web:d08d53cddb886933d34433"
 };
 
 // app => objeto com todas as configurações do Firebase
